9003f33cc656c107b1c2f4f4d7fcd7980032005a
dell <dell@DESKTOP-KTJNK95>
2021-09-14 21:12:03 +0530

    commit

--- inheritancedemo/Program.cs
+++ inheritancedemo/Program.cs
@@ -6,10 +6,54 @@ using System.Threading.Tasks;
 
 namespace inheritancedemo
 {
+    class animal
+    {
+        //Implimentation of virtual method
+        public virtual void FoodHabbits()
+        {
+            Console.WriteLine("Animals with different food habbits");
+        }
+    }
+
+    class carnivours:animal
+    {
+        public override void FoodHabbits()
+        {
+            Console.WriteLine("Animals eat meat");
+        }
+    }
+
+    class herbivours : animal
+    {
+        public override void FoodHabbits()
+        {
+            Console.WriteLine("Animals eat grass");
+        }
+    }
+
+    class implement
+    {
+        public static void callfun(animal obj)
+        {
+            obj.FoodHabbits();
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            animal a = new animal();
+            herbivours h = new herbivours();
+            carnivours c = new carnivours();
+            //static binding/early binding
+            a.FoodHabbits();
+            h.FoodHabbits();
+            c.FoodHabbits();
+            //dynamic binding/late binding
+            implement.callfun(h);
+            Console.ReadLine();
         }
     }
 }
